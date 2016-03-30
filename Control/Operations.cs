58e7097e713f0ce4adfb133ffa2ba9ae5cfc335a
Joshua Broughton <joshua.c.broughton@gmail.com>
2016-03-30 16:32:49 -0400

    Built solution, added .gitignore with most common VS temp files, build files and a bunch of common addons.

--- Control/Operations.cs
+++ Control/Operations.cs
@@ -14,6 +14,7 @@ namespace VisualJoshulator.Control
         {
             return val1 + val2; 
         }
+        
         public static double Subtract(double val1, double val2)
         {
             return val1 - val2;
