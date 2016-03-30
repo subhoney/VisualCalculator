e576e0b3ed195405a1bd2881d5f65ef7f9f6bae3
Joshua Broughton <joshua.c.broughton@gmail.com>
2016-03-30 16:07:21 -0400

    Initial commit. Full UI design.

---
+++ Control/Operations.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualJoshulator.Control
+{
+    class Operations
+    {
+
+        //All operations in this class are bogus. 
+        public static double Add(double val1, double val2)
+        {
+            return val1 + val2; 
+        }
+        public static double Subtract(double val1, double val2)
+        {
+            return val1 - val2;
+        }
+        public static double Mod(double val1, double val2)
+        {
+            return val1 % val2;
+        }
+        public static double Pow(double val1, double val2)
+        {
+            return Math.Pow(val1, val2); 
+        }
+        public static double Divide(double val1, double val2)
+        {
+            double result = 0; 
+            try
+            {
+               result = val1 / val2;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message.ToString()); 
+            }
+            return result;
+        }
+    }
+}
---
+++ Model/CalculationFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualJoshulator.Model
+{
+    class CalculationFactory
+    {
+        private List<string> userInput;
+
+        public List<string> UserInput
+        {
+            get
+            {
+                return userInput;
+            }
+
+            set
+            {
+                userInput = value;
+            }
+        }
+    }
+}
